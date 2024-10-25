c32c265e2baa31d83f470806e9b2595e0e8886eb
wasimhassanshah <sayedwasimshahkazmi@gmail.com>
2024-10-25 18:22:28 +0500

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
-import logging
 import traceback
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -19,10 +19,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1 / 0  # This will raise a ZeroDivisionError
-    except Exception as e:
-        logging.error("An error occurred: %s", str(e))
-        logging.error("Full traceback: %s", traceback.format_exc())  # Log the full traceback
-        raise CustomException(str(e), sys)  # Raise with the error message and sys details
+ 
